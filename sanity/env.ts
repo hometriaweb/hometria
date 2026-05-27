5740c19063a891a532b3fea0cd5f4812401406e2
Bartłomiej Ćwiklak <62007413+bartlomiejcwiklak@users.noreply.github.com>
2026-05-27 11:55:14 +0200

    fix: add fallbacks for sanity env vars to fix vercel build

--- sanity/env.ts
+++ sanity/env.ts
@@ -2,14 +2,15 @@ export const apiVersion =
   process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2026-05-27'
 
 export const dataset = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_DATASET,
+  process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
   'Missing environment variable: NEXT_PUBLIC_SANITY_DATASET'
 )
 
 export const projectId = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'b6yvfjh2',
   'Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID'
 )
+
 
 function assertValue<T>(v: T | undefined, errorMessage: string): T {
   if (v === undefined) {
